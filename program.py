fe2e69aa7e74225aae85f41e09219406ead0aaf4
Charles2205 <cewudzie0@gmail.com>
2023-02-21 12:01:58 +0000

    Implemented student database

---
+++ program.py
@@ -0,0 +1,6 @@
+from student import Student
+from system_colors import GREEN,YELLOW,RED,GREEN
+
+# Creating a database from a list
+student_db=[]
+student_id = 's'
--- student.py
+++ student.py
@@ -1,2 +1,10 @@
 class Student:
     """ A class to create students object for the system"""
+    def __init__(self,id,first_name,last_name,dob,gender,course):
+        self.id =id
+        self.first_name=first_name
+        self.last_name =last_name
+        self.dob =dob
+        self.gender = gender
+        self.course = course
+        
